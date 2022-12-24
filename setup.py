a071c02a443600c8fa1c76ac0751521da81c3571
fedefliguer <56199576+fedefliguer@users.noreply.github.com>
2022-12-24 16:05:56 -0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+from chartlevels import __version__
+
+setup(
+    name='chartlevels',
+    version=__version__,
+
+    url='https://github.com/fedefliguer/chartlevels',
+    author='Federico Fliguer',
+    author_email='federico.fliguer@gmail.com',
+
+    py_modules=['chartlevels'],
+)
